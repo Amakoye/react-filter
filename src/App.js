42c981a48c0c424e31f34a919936a7502b0fd32c
Amakoye <charlesamakoye@gmail.com>
2022-09-12 15:11:07 +0300

    advanced search with multiple values

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
 import React from "react";
 import "./App.css";
+import Table from "./Table";
 import { Users } from "./users";
 
 function App() {
   const [query, setQuery] = React.useState("");
-  console.log(query);
+
+  const keys = ["first_name", "last_name", "email"];
+
+  const search = (data) => {
+    return data.filter((item) =>
+      keys.some((key) => item[key].toLowerCase().includes(query.toLowerCase()))
+    );
+    /*
+    not the best way
+
+    return data.filter(
+      (item) =>
+        item.first_name.toLowerCase().includes(query.toLowerCase()) ||
+        item.last_name.toLowerCase().includes(query.toLowerCase()) ||
+        item.email.toLowerCase().includes(query.toLowerCase())
+    ); */
+  };
+
   return (
     <div className="app">
       <input
@@ -13,15 +31,7 @@ function App() {
         className="search"
         onChange={(e) => setQuery(e.target.value)}
       />
-      <ul className="list">
-        {Users.filter((user) =>
-          user.first_name.toLowerCase().includes(query.toLowerCase())
-        ).map((user) => (
-          <li key={user.id} className="listItem">
-            {user.first_name}
-          </li>
-        ))}
-      </ul>
+      <Table data={search(Users)} />
     </div>
   );
 }

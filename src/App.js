bddbd7b58f9ba06aae63ec20095eeab6bd9e931b
Amakoye <charlesamakoye@gmail.com>
2022-09-12 14:38:27 +0300

    basic search

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
+import React from "react";
 import "./App.css";
+import { Users } from "./users";
 
 function App() {
-  return <div className="App"></div>;
+  const [query, setQuery] = React.useState("");
+  console.log(query);
+  return (
+    <div className="app">
+      <input
+        type="text"
+        placeholder="Search...."
+        className="search"
+        onChange={(e) => setQuery(e.target.value)}
+      />
+      <ul className="list">
+        {Users.filter((user) =>
+          user.first_name.toLowerCase().includes(query.toLowerCase())
+        ).map((user) => (
+          <li key={user.id} className="listItem">
+            {user.first_name}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default App;
